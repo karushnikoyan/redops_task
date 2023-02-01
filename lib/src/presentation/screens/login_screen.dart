27808f49a7e439fa47536c9337b3568fffb87f9f
Karush <nikoyankarush@gmail.com>
2023-02-01 23:39:46 +0400

    config

---
+++ lib/src/presentation/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(height: 444,
+            decoration: BoxDecoration(),),
+          Container(height: 300,color: Colors.blue,),
+        ],
+      ),
+
+    );
+  }
+}
